18a6b06355457b46b25b84b57e2c7b91160849b0
qaqaqaqaowh <nicholasowh@hotmail.com>
2023-06-13 11:23:15 +0800

    remove test code

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,5 @@
 <template>
 	<v-app id="inspire">
-		<div id="pwa">this is pwa</div>
-		<div id="browser">this is browser</div>
 		<!-- style="background: #dcdcdc"-->
 		<v-navigation-drawer
 			dark
